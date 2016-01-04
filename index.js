b2ef02d42d71ea1d5635570f04df8017000ed43e
Jason Anderson <diurnalist@gmail.com>
2016-01-04 15:24:44 +0100

    Update tests

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-var loaderUtils = require("loader-utils");
-var createCompiler = require("./lib/compiler");
+var loaderUtils = require('loader-utils');
+var createCompiler = require('./lib/compiler');
 
-console.log('wtf');
+function createInlineDetectRegex(part) {
+	// Create a regex that is used to operate over items in a Handlebars compiler buffer.
+	// This creates a regex that consists of a few capture groups:
+	// 1 - detect whether it's a double-quoted inline require pattern (like `"<img src=\"path/to/required/image\">"`)
+	// 2 - wrap whatever was provided in options and add a pattern to capture the rest of a require pattern
+	// e.g. given an input pattern of "dev-stuff", it should match:
+	//   "dev-stuff/path/to/thing.jpg"
+	//   "dev-stuff/thing"
+	//   "<img src=\"dev-stuff/path/to/thing.jpg\">"
+	return new RegExp('(\\\\?)"((?:' + part + ')(?:\\/[\\w\\-\\.]+)*)(?:\\\\?)"', 'g');
+}
 
 module.exports = function(source) {
 	if (this.cacheable) this.cacheable();
 
-	console.log('HANDLEBARS LOADER');
-
 	var query = this.query instanceof Object ? this.query : loaderUtils.parseQuery(this.query);
 
-	var runtimePath = query.runtime || require.resolve("handlebars/runtime");
-	var inline = query.inline && new RegExp(query.inline, 'g');
+	var runtimePath = query.runtime || require.resolve('handlebars/runtime');
+	var inlines = query.inlines && createInlineDetectRegex(query.inlines);
 
-	var compiler = createCompiler(require(runtimePath), inline);
+	var compiler = createCompiler(require(runtimePath), inlines);
 	var template = compiler(source);
-
-	console.log(template);
 
 	// export as module if template is not blank
 	return template
--- test/test.js
+++ test/test.js
@@ -21,9 +21,14 @@ function applyTemplate(source, options) {
       }),
       _module = {};
 
-  (new Function('module', 'require', source))(_module, _require);
-
-  options.test(null, _module.exports(options.data), _require);
+  try {
+    // evaluate the template file
+    (new Function('module', 'require', source))(_module, _require);
+    // execute template with data
+    options.test(null, _module.exports(options.data), _require);
+  } catch (err) {
+    options.test(err);
+  }
 }
 
 function loadTemplate(templateName) {
@@ -31,30 +36,27 @@ function loadTemplate(templateName) {
 }
 
 function testTemplate(loader, template, options, testFn) {
-  var resolveStubs = {};
+  var resolveStubs = {}, loadedTemplate;
 
   for (var k in options.stubs) {
     resolveStubs[k] = k;
   }
 
-  loader.call(new WebpackLoaderMock({
-    query: options.query,
-    resolveStubs: resolveStubs,
-    async: function (err, source) {
-      if (err) {
-        // Proxy errors from loader to test function
-        return testFn(err);
-      } else if (!source) {
-        return testFn(new Error('Could not generate template'));
-      }
+  try {
+    var loadedTemplate = loader.call(new WebpackLoaderMock({
+      query: options.query,
+      resolveStubs: resolveStubs
+    }), loadTemplate(template));
+  } catch (err) {
+    // exit early
+    return testFn(err);
+  }
 
-      applyTemplate(source, {
-        data: options.data,
-        requireStubs: options.stubs,
-        test: testFn
-      });
-    }
-  }), loadTemplate(template));
+  applyTemplate(loadedTemplate, {
+    data: options.data,
+    requireStubs: options.stubs,
+    test: testFn
+  });
 }
 
 function getStubbedHandlebarsTemplateFunction() {
@@ -77,139 +79,25 @@ describe('handlebars-loader', function () {
     });
   });
 
-  it('should not require handlebars for empty templates', function (done) {
-    testTemplate(loader, 'empty', {
-      data: TEST_TEMPLATE_DATA
-    }, function (err, output, require) {
-      assert.equal(require.callCount, 0);
-      done();
-    });
-  });
-
-  it('should convert helpers into require statements', function (done) {
+  it('supports module-style helpers', function (done) {
     testTemplate(loader, 'with-helpers', {
+      data: TEST_TEMPLATE_DATA,
       stubs: {
-        'title': function (text) { return 'Title: ' + text; },
-        './description': function (text) { return 'Description: ' + text; }
-      },
-      data: TEST_TEMPLATE_DATA
+        'title': function (text) { return 'Title: ' + text; }
+      }
     }, function (err, output, require) {
       assert.ok(output, 'generated output');
       assert.ok(require.calledWith('title'),
-        'should have loaded helper with module syntax');
-      assert.ok(require.calledWith('./description'),
-        'should have loaded helper with relative syntax');
-      done();
-    });
-  });
-
-  it('should convert partials into require statements', function (done) {
-    testTemplate(loader, 'with-partials', {
-      stubs: {
-        './partial': require('./templates/partial.handlebars'),
-        'partial': require('./templates/partial.handlebars')
-      },
-      data: TEST_TEMPLATE_DATA
-    }, function (err, output, require) {
-      assert.ok(output, 'generated output');
-      assert.ok(require.calledWith('partial'),
-        'should have loaded partial with module syntax');
-      assert.ok(require.calledWith('./partial'),
-        'should have loaded partial with relative syntax');
-      done();
-    });
-  });
-
-  it('allows specifying additional helper search directory', function (done) {
-    testTemplate(loader, 'with-dir-helpers', {
-      query: '?helperDirs[]=' + path.join(__dirname, 'helpers'),
-      data: TEST_TEMPLATE_DATA
-    }, function (err, output, require) {
-      assert.ok(output, 'generated output');
-      done();
-    });
-  });
-
-  it('allows specifying multiple additional helper search directories', function (done) {
-    testTemplate(loader, 'with-dir-helpers-multiple', {
-      query: '?helperDirs[]=' + path.join(__dirname, 'helpers') + '&helperDirs[]=' + path.join(__dirname, 'helpers2'),
-      data: TEST_TEMPLATE_DATA
-    }, function (err, output, require) {
-      assert.ok(output, 'generated output');
-      done();
-    });
-  });
-
-  it('allows specifying multiple additional helper search directories with object literal', function (done) {
-    testTemplate(loader, 'with-dir-helpers-multiple', {
-      query: {
-        helperDirs: [
-          path.join(__dirname, 'helpers'),
-          path.join(__dirname, 'helpers2')
-        ]
-      },
-      data: TEST_TEMPLATE_DATA
-    }, function (err, output, require) {
-      assert.ok(output, 'generated output');
-      done();
-    });
-  });
-
-  it('allows specifying multiple additional helper search directories with JSON', function (done) {
-    testTemplate(loader, 'with-dir-helpers-multiple', {
-      query: '?' + JSON.stringify({
-        helperDirs: [
-          path.join(__dirname, 'helpers'),
-          path.join(__dirname, 'helpers2')
-        ]
-      }),
-      data: TEST_TEMPLATE_DATA
-    }, function (err, output, require) {
-      assert.ok(output, 'generated output');
-      done();
-    });
-  });
-
-  it('should find helpers in subdirectories', function (done) {
-    testTemplate(loader, 'with-nested-helper', {
-      query: '?helperDirs[]=' + path.join(__dirname, 'helpers'),
-      data: TEST_TEMPLATE_DATA
-    }, function (err, output, require) {
-      assert.ok(output, 'generated output');
-      done();
-    });
-  });
-
-  it('allows changing where implicit helpers and partials resolve from', function (done) {
-    function testWithRootRelative(rootRelative, next) {
-      var stubs = {},
-          relativeHelper = rootRelative + 'description';
-
-      stubs['title'] = function (text) { return 'Title: ' + text; };
-      stubs[relativeHelper] = function (text) { return 'Description: ' + text; };
-
-      testTemplate(loader, 'with-helpers', {
-        query: '?rootRelative=' + rootRelative,
-        stubs: stubs,
-        data: TEST_TEMPLATE_DATA
-      }, function (err, output, require) {
-        assert.ok(output, 'generated output');
-        assert.ok(require.calledWith(relativeHelper), 'required helper at ' + relativeHelper);
-        next();
-      });
-    }
-
-    async.each(['./', '../', '', 'path/to/stuff'], testWithRootRelative, function (err, results) {
-      assert.ok(!err, 'no errors');
+        'should have required title helper');
       done();
     });
   });
 
   it('allows specifying inline requires', function (done) {
     testTemplate(loader, 'with-inline-requires', {
-      query: '?inlineRequires=^images\/',
+      query: '?inlines=images',
       stubs: {
-        './image': function (text) { return 'Image URL: ' + text; },
+        'image': function (text) { return 'Image URL: ' + text; },
         'images/path/to/image': 'http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'
       }
     }, function (err, output, require) {
@@ -276,102 +164,9 @@ describe('handlebars-loader', function () {
   });
 
   it('properly catches errors when unknown helper found', function (done) {
-    testTemplate(loader, 'invalid-unknown-helpers', {
-      stubs: {
-        // A two-pass compile is required to see this error, and two-pass compilation
-        // only happens when the loader finds SOME helper/partial during the first pass.
-        // So we stub one that it can find.
-        //
-        // This means that if your template ONLY contains an unknown helper, the loader will not
-        // detect an error, and you will only see problems when you attempt to render the template.
-        //
-        // TODO: figure out better way to detect helpers so the resolveHelpersIterator can reliably
-        // catch errors, instead of assuming that the helper is actually a template var
-        './unknownExistingHelper': function (text) { return text; }
-      }
-    }, function (err, output, require) {
+    testTemplate(loader, 'invalid-unknown-helpers', {}, function (err, output, require) {
       assert.ok(err, 'got error');
-      assert.ok(err.message.indexOf('You specified knownHelpersOnly') >= 0, 'error was handlebars unknown helper error');
-      done();
-    });
-  });
-
-  it('allows specifying known helpers', function (done) {
-    var runtimePath = require.resolve('handlebars/runtime');
-    var stubs = {};
-
-    // Need to set up a stubbed handlebars runtime that has our known helper loaded in
-    var Handlebars = require('handlebars/runtime').default.create();
-    stubs[runtimePath] = Handlebars;
-    Handlebars.registerHelper('someKnownHelper', function () {
-      return 'some known helper';
-    });
-
-    testTemplate(loader, 'with-known-helpers', {
-      query: '?knownHelpers[]=someKnownHelper',
-      stubs: stubs
-    }, function (err, output, require) {
-      assert.ok(output, 'generated output');
-      assert.ok(output.indexOf('some known helper') >= 0);
-      assert.ok(!require.calledWith('./someKnownHelper'), 'should not have tried to dynamically require helper');
-      done();
-    });
-  });
-
-  it('should find helpers and partials if both relativeRoot and helperDirs is set', function (done) {
-    var rootRelative = path.resolve(__dirname, 'relativeRoot') + '/';
-    var helperDirs = path.join(__dirname, 'helpers');
-    var stubs = {
-      'relative-partial': require('./relativeRoot/relative-partial.handlebars'),
-      'image': require('./helpers/image.js'),
-      'nested/quotify': require('./helpers/nested/quotify.js')
-    };
-    stubs[rootRelative + 'relative-partial'] = require('./relativeRoot/relative-partial.handlebars');
-
-    testTemplate(loader, 'with-relative-root', {
-      stubs: stubs,
-      query: '?helperDirs[]=' + helperDirs + '&rootRelative=' + rootRelative,
-      data: TEST_TEMPLATE_DATA
-    }, function (err, output, require) {
-      assert.ok(output, 'generated output');
-      assert.ok(require.calledWith('image'),
-        'should have loaded helper');
-
-      assert.ok(require.calledWith('nested/quotify'),
-        'should have loaded nested helper');
-
-      assert.ok(require.calledWith('relative-partial'),
-        'should have loaded partial with module syntax');
-      assert.ok(require.calledWith(rootRelative + 'relative-partial'),
-        'should have loaded partial with relative syntax');
-      done();
-    });
-  });
-
-  it('should find helpers and partials if inlineRequires is set', function (done) {
-    testTemplate(loader, 'with-partials-helpers-inline-requires', {
-      query: '?inlineRequires=^images\/',
-      stubs: {
-        './partial': require('./templates/partial.handlebars'),
-        'partial': require('./templates/partial.handlebars'),
-        'title': function (text) { return 'Title: ' + text; },
-        './description': function (text) { return 'Description: ' + text; },
-        './image': function (text) { return 'Image URL: ' + text; },
-        'images/path/to/image': 'http://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50'
-      },
-      data: TEST_TEMPLATE_DATA
-    }, function (err, output, require) {
-      assert.ok(output, 'generated output');
-      assert.ok(require.calledWith('partial'),
-        'should have loaded partial with module syntax');
-      assert.ok(require.calledWith('./partial'),
-        'should have loaded partial with relative syntax');
-      assert.ok(require.calledWith('title'),
-        'should have loaded helper with module syntax');
-      assert.ok(require.calledWith('./description'),
-        'should have loaded helper with relative syntax');
-      assert.ok(require.calledWith('images/path/to/image'),
-        'should have required image path');
+      assert.ok(err.message.indexOf('Missing helper') >= 0, 'error was handlebars unknown helper error');
       done();
     });
   });
